7f37d3474e5d8c6b4f03fe1a73c6203802540973
Austen Herrick <Austen Herrick>
2017-02-08 02:20:04 -0800

    Offensive Strategy Built

--- GomokuPlayer.java
+++ GomokuPlayer.java
@@ -5,9 +5,17 @@ import java.net.UnknownHostException;
 
 public class GomokuPlayer {
 
+	private final static int JOINT  = 3; //Magic Number for secret weapon
+	private static boolean cont = true;
+	private static boolean phase1 = true;
+	private static boolean phase2 = false;
+	private static boolean phase3 = false;
+	private static int secretX = 0;
+	private static int secretY; //DEPENDS on size of board
 	private static char player;
 	private static int boardSize;
 	private static char[][] board;
+	private static int[][] currMove;
 	
 	//in and out protocol
 	private DataOutputStream dOut;
@@ -27,12 +35,90 @@ public class GomokuPlayer {
 	
 	public String gomokuMain(char turn) throws IOException, UnknownHostException
 	{
+		secretY = boardSize - JOINT - 1;
 		String status = "continuing";
+		int i;
+		int j;
+		int xCount = 0;
+		int oCount = 0;
+		int xSize = boardSize * boardSize;
+		int oSize = boardSize * boardSize;
+		System.out.println("xSize = " + xSize);
+		System.out.println("oSize = " + oSize);
+		int [] xFound;
+		int [] oFound;
+		xFound = new int[xSize];
+		oFound = new int[xSize];
+		do {
+			for (i=0; i < boardSize; i++) {
+				for (j=0; j < boardSize; j++) {
+					if (board[i][j] == 'x') {
+						System.out.println(i + " " + j + " " + "Found " + board[i][j]);
+						if (xCount < (xSize / 2)) {
+							xFound[xCount] = i;
+							xCount++;
+							xFound[xCount] = j;
+							xCount++;
+							System.out.println(i + " " + j);
+						}
+					}
+					else if (board[i][j] == 'o') {
+						System.out.println(i + " " + j + " " + "Found " + board[i][j]);
+						if (oCount < (xSize / 2)) {
+							oFound[oCount] = i;
+							oCount++;
+							oFound[oCount] = j;
+							oCount++;
+							System.out.println(i + " " + j);
+						}
+					}
+				}
+			}
+			if (cont) {
+				System.out.println("Secret Mode");
+				if (phase1 || phase2) {
+					System.out.println("Beginning phases Secret Mode");
+					makeMove(secretY, secretX);
+				}
+				if (phase2) {
+					secretY--;
+				}
+				if (secretX == JOINT -1) {
+					phase1 = false;
+					phase2 = true;
+					secretX = JOINT;
+					secretY = boardSize - 1;
+				}
+				if (secretY == JOINT -1) {
+					phase2 = false;
+					secretY = JOINT;
+				}
+				if (phase1) {
+					secretX++;
+				}
+				if (!phase1 && !phase2) {
+					System.out.println("Completing Secret mode");
+					makeMove(secretY, secretX);
+					phase3 = true;
+				}
+				if (phase3) {
+					System.out.println("Winning in Secret mode");
+					if (board[secretY + 1][secretX] == ' ') {
+						makeMove(secretX + 1, secretY);
+					}
+					else if (board[secretY][secretX + 1] == ' ') {
+						makeMove(secretY, secretX + 1);
+					}
+					else {
+						System.out.println("Shouldn't have made it thus far!!!");
+					}
+				}
+			}
+			status = readServer();
+			System.out.println(status);
+		} while (status.equals("continuing"));
 		
-		//if(turn != player)
-			//return "error";
-		
-		do
+		/*do
 		{
 			boolean random = true;
 			
@@ -52,7 +138,7 @@ public class GomokuPlayer {
 			status = readServer();
 			System.out.println(status);
 			
-		} while(status.equals("continuing"));
+		} while(status.equals("continuing"));*/
 		
 		return status;
 	}

3624f7f50d9f91e1a5b91548e869deaff0888734
Austen Herrick <Austen Herrick>
2017-02-09 03:24:32 -0800

    Heuristic function printing pieces encountered and moved secret weapon to function.

--- GomokuPlayer.java
+++ GomokuPlayer.java
@@ -4,19 +4,19 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class GomokuPlayer {
-
-	private final static int JOINT  = 3; //Magic Number for secret weapon
-	private static boolean cont = true;
-	private static boolean phase1 = true;
-	private static boolean phase2 = false;
-	private static boolean phase3 = false;
-	private static int secretX = 0;
-	private static int secretY; //DEPENDS on size of board
-	private static char player;
+	/* Whole game */
 	private static int boardSize;
 	private static char[][] board;
 	private static int[][] currMove;
-	
+	private static int count = 0;
+	private static String status = "continuing";
+	/*secret weapon*/
+	private final static int Sarr[] = {7, 0, 7, 1, 7, 4, 10, 3, 9, 3, 6, 3, 7, 3, 7, 4, 6, 3, 7, 3};
+	private final static int JOINT  = 3; //Magic Number for secret weapon
+	private static boolean cont = true;
+	private static int sRow;
+	private static int sCol = 0; //DEPENDS on size of board
+	private static char player;
 	//in and out protocol
 	private DataOutputStream dOut;
 	private BufferedReader in;
@@ -30,94 +30,25 @@ public class GomokuPlayer {
 		
 		this.in = in;
 		this.dOut = dOut;
-		
 	}
 	
 	public String gomokuMain(char turn) throws IOException, UnknownHostException
-	{
-		secretY = boardSize - JOINT - 1;
-		String status = "continuing";
-		int i;
-		int j;
-		int xCount = 0;
-		int oCount = 0;
-		int xSize = boardSize * boardSize;
-		int oSize = boardSize * boardSize;
-		System.out.println("xSize = " + xSize);
-		System.out.println("oSize = " + oSize);
-		int [] xFound;
-		int [] oFound;
-		xFound = new int[xSize];
-		oFound = new int[xSize];
+	{	
+		sRow = boardSize - JOINT - 1;
 		do {
-			for (i=0; i < boardSize; i++) {
-				for (j=0; j < boardSize; j++) {
-					if (board[i][j] == 'x') {
-						System.out.println(i + " " + j + " " + "Found " + board[i][j]);
-						if (xCount < (xSize / 2)) {
-							xFound[xCount] = i;
-							xCount++;
-							xFound[xCount] = j;
-							xCount++;
-							System.out.println(i + " " + j);
-						}
-					}
-					else if (board[i][j] == 'o') {
-						System.out.println(i + " " + j + " " + "Found " + board[i][j]);
-						if (oCount < (xSize / 2)) {
-							oFound[oCount] = i;
-							oCount++;
-							oFound[oCount] = j;
-							oCount++;
-							System.out.println(i + " " + j);
-						}
-					}
-				}
+		//Will accept heuristic return in future, Heuristic will return a false boolean if secret weapon is not optimal
+		boolean heuristic = true;
+		heuristic(); // will perform alpha beta pruning and set global variables based on best results
+		System.out.println(status);
+		if (heuristic)
+			if (secretWeapon()) {
+				if (board[7][2] == ' ')
+					makeMove(7, 2);
+				else
+					makeMove(8, 3);
 			}
-			if (cont) {
-				System.out.println("Secret Mode");
-				if (phase1 || phase2) {
-					System.out.println("Beginning phases Secret Mode");
-					makeMove(secretY, secretX);
-				}
-				if (phase2) {
-					secretY--;
-				}
-				if (secretX == JOINT -1) {
-					phase1 = false;
-					phase2 = true;
-					secretX = JOINT;
-					secretY = boardSize - 1;
-				}
-				if (secretY == JOINT -1) {
-					phase2 = false;
-					secretY = JOINT;
-				}
-				if (phase1) {
-					secretX++;
-				}
-				if (!phase1 && !phase2) {
-					System.out.println("Completing Secret mode");
-					makeMove(secretY, secretX);
-					phase3 = true;
-				}
-				if (phase3) {
-					System.out.println("Winning in Secret mode");
-					if (board[secretY + 1][secretX] == ' ') {
-						makeMove(secretX + 1, secretY);
-					}
-					else if (board[secretY][secretX + 1] == ' ') {
-						makeMove(secretY, secretX + 1);
-					}
-					else {
-						System.out.println("Shouldn't have made it thus far!!!");
-					}
-				}
-			}
-			status = readServer();
-			System.out.println(status);
+		status = readServer();
 		} while (status.equals("continuing"));
-		
 		/*do
 		{
 			boolean random = true;
@@ -139,8 +70,73 @@ public class GomokuPlayer {
 			System.out.println(status);
 			
 		} while(status.equals("continuing"));*/
-		
+
 		return status;
+	}
+	
+	private void heuristic() {
+		int i;
+		int j;
+		int xCount = 0;
+		int oCount = 0;
+		int xSize = boardSize * boardSize;
+		int oSize = boardSize * boardSize;
+		int [] xFound  = new int[xSize];
+		int [] oFound = new int[xSize];
+		System.out.println("xSize = " + xSize);
+		System.out.println("oSize = " + oSize);
+		for (i=0; i < boardSize; i++) {
+			for (j=0; j < boardSize; j++) {
+				if (board[i][j] == 'x') {
+					System.out.println(i + " " + j + " " + "Found " + board[i][j]);
+					if (xCount < (xSize / 2)) {
+						System.out.println("Our array holds " + (xSize/2) + " x's");
+						xFound[xCount] = i;
+						xCount++;
+						xFound[xCount] = j;
+						xCount++;
+						System.out.println(i + " " + j);
+						System.out.println("This is the " + (xCount/2)  + " x we've encountered");
+					}
+				}
+				else if (board[i][j] == 'o') {
+					if (oCount < (xSize / 2)) {
+						oFound[oCount] = i;
+						oCount++;
+						oFound[oCount] = j;
+						oCount++;
+						System.out.println(i + " " + j);
+						System.out.println("This is the " + (oCount/2)  + " o we've encountered");
+					}
+				}
+			}
+		}
+	}
+	
+	private boolean secretWeapon() {
+		if (board[7][3] == player) {
+			return true;//Reached goal
+		}
+		else {
+			System.out.println("At board position " + sRow + " " + sCol);
+			if (count < Sarr.length) {
+				System.out.println("Lenght of array: " + Sarr.length + "vs. " + count);
+			sRow = Sarr[count];
+			count++;
+			sCol = Sarr[count];
+			count++;
+			}
+			try {
+				makeMove(sRow, sCol);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return false;
 	}
 	
 	private String readServer() throws IOException, UnknownHostException
